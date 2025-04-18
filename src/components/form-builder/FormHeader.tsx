3be25cd5797f62fd397cc7946e29f530389136d5
gazinafis007 <gazinafisrafi.gnr@gmail.com>
2025-04-18 08:00:58 +0600

    logo added

--- src/components/form-builder/FormHeader.tsx
+++ src/components/form-builder/FormHeader.tsx
@@ -20,12 +20,13 @@ export default function FormHeader() {
     <header className="bg-white border-b border-neutral-200 py-3 px-4 flex justify-between items-center">
       <div className="flex items-center">
         <div className="flex items-center mr-2">
-          <svg width="36" height="36" viewBox="0 0 36 36" fill="none" xmlns="http://www.w3.org/2000/svg" className="h-9 w-9 rounded-lg">
+            <img className='w-10 h-10' src='/images/wempro_com_logo.jpg' alt='wempro_logo'/>
+          {/* <svg width="36" height="36" viewBox="0 0 36 36" fill="none" xmlns="http://www.w3.org/2000/svg" className="h-9 w-9 rounded-lg">
             <rect width="36" height="36" rx="8" fill="#F7F7F9"/>
             <path d="M18 8C12.477 8 8 12.477 8 18C8 23.523 12.477 28 18 28C23.523 28 28 23.523 28 18C28 12.477 23.523 8 18 8Z" fill="#4A6CF7"/>
             <path d="M22 14H14V22H22V14Z" fill="#FF564D"/>
             <path d="M26 10H18V18H26V10Z" fill="#00D084"/>
-          </svg>
+          </svg> */}
         </div>
         <div>
           <h1 className="text-lg font-bold">Form Builder</h1>
